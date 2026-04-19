ffdb2e9d00e391ee9aaf2496dc80034ad0ab4a6d
Antonio <antoniovila.dev@gmail.com>
2026-04-19 00:15:06 -0400

    feat: add CLI entry point
    Wires all commands into the Cobra root and sets build-time
    version and commit variables via ldflags.

---
+++ cmd/kip/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kipenv/kip/internal/cli"
+)
+
+func main() {
+	if err := cli.NewRootCmd().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
